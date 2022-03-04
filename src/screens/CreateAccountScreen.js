62d8bdbe92d94aedb2d2f8885dfcb65146f9fd14
duanduanzhou <98435844+duanduanzhou@users.noreply.github.com>
2022-03-04 00:46:09 -0800

    Add files via upload

---
+++ src/screens/CreateAccountScreen.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import LoginScreen from './src/screens/LoginScreen';
+import HomeScreen from './src/screens/HomeScreen';
+import CreateAccountScreen from './src/screens/CreateAccountScreen';
+
+const AppNavigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Login: LoginScreen,
+    CreateAccount: CreateAccountScreen,
+  },
+  {
+    initialRouteName: 'Login',
+  },
+);
+
+export default createAppContainer(AppNavigator);
