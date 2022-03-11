8cd93aa23e130d82393ed5d87a57b2f12225ae62
nicholas-bui <85708868+nicholas-bui@users.noreply.github.com>
2022-03-10 21:21:46 -0800

    updated code

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image,TouchableOpacity,StyleSheet, Text, View } from 'react-native';
 
 
 import { NavigationContainer } from '@react-navigation/native';
@@ -9,36 +9,52 @@ import LoginScreen from './util/LoginScreen';
 import HomeScreen from './util/HomeScreen';
 import CreateAccountScreen from './util/CreateAccountScreen';
 import DocumentSelectScreen from './util/DocumentSelectScreen';
+import ProfileScreen from './util/ProfileScreen'
 
-/*const AppNavigator = createStackNavigator(
-  {
-    Home: HomeScreen,
-    Login: LoginScreen,
-    CreateAccount: CreateAccountScreen,
-    DocumentSelect: DocumentSelectScreen,
-  },
-  {
-    initialRouteName: 'Login',
-  },
-);
-*/
-//import { createAppContainer } from 'react-navigation';
-//import { createStackNavigator } from 'react-navigation-stack';
+const IMAGES = {
+  facebooklogo: require('./warn.png'),
+};
 
 const Stack = createNativeStackNavigator();
 
 function MyStack() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="DocumentSelect" options={{title: 'Document Select',}} component={DocumentSelectScreen} />
-      <Stack.Screen name="CreateAccount" component={CreateAccountScreen} />
+      <Stack.Screen name="Login"options={{headerShown: false}} component={LoginScreen} />
+      <Stack.Screen name="Home" options={{headerShown: false}} component={HomeScreen} />
+      <Stack.Screen name="DocumentSelect"
+      options={{
+          title: "",
+          headerBackTitle:"Logout",
+          headerStyle: {
+            backgroundColor: '#2c2c2c',
+            height: 100,
+          },
+          headerRight: () => (
+
+            <TouchableOpacity style={styles.Button} onPress={() => alert('This is a button!')} >
+              <Image
+                style={styles.buttonLogo}
+                source={IMAGES.facebooklogo}/>
+            </TouchableOpacity>
+
+          ),
+        }}
+      component={DocumentSelectScreen} />
+      <Stack.Screen name="CreateAccount" options={{headerShown: false}} component={CreateAccountScreen} />
+      <Stack.Screen name="Profile" 
+      options={{
+        title: "",
+        headerBackTitle:"Home",
+        headerStyle: {
+          backgroundColor: '#2c2c2c',
+          height: 100,
+        },
+      }}
+      component={ProfileScreen} />
     </Stack.Navigator>
   );
 }
-
-//export default createAppContainer(AppNavigator);
 
 export default function App() {
   return (
@@ -47,3 +63,13 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+
+  buttonLogo: {
+     width: 30,
+     height: 30,
+   },
+
+
+});
